c210a6519ff2c32e9c1d0f4447c225b15a7e28e4
OscarAntona <53609303+OscarAntona@users.noreply.github.com>
2022-11-17 10:25:15 +0100

    Detail view

--- app/src/main/java/com/antgut/moviesapp/presentation/FactoryMovies.kt
+++ app/src/main/java/com/antgut/moviesapp/presentation/FactoryMovies.kt
@@ -6,17 +6,30 @@ import com.antgut.moviesapp.data.DataRepository
 import com.antgut.moviesapp.data.local.room.RoomLocalDataSource
 import com.antgut.moviesapp.data.remote.retrofit.ApiClient
 import com.antgut.moviesapp.data.remote.retrofit.RetrofitRemoteDataSource
+import com.antgut.moviesapp.domain.GetMovieUseCase
 import com.antgut.moviesapp.domain.GetMoviesUseCase
 
-class Factory {
-    fun getViewModel(applicationContext: Context):ViewModel{
-        return ViewModel(
+class FactoryMovies {
+    fun getViewModel(applicationContext: Context):FeedViewModel{
+        return FeedViewModel(
             GetMoviesUseCase(
                 DataRepository(
                     RoomLocalDataSource(
                         AppDataBase.getDataBase(applicationContext).daoMovies(),
                     ),
                     RetrofitRemoteDataSource(ApiClient()),
+                )
+            )
+        )
+    }
+    fun getDetailViewModel(applicationContext: Context):DetailViewModel{
+        return DetailViewModel(
+            GetMovieUseCase(
+                DataRepository(
+                    RoomLocalDataSource(
+                        AppDataBase.getDataBase(applicationContext).daoMovies(),
+                    ),
+                   RetrofitRemoteDataSource(ApiClient()),
                 )
             )
         )
